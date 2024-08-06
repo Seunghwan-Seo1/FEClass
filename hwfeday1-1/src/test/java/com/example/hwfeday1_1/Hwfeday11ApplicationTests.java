9ff0f5a75fe6e8d1a11f6b63ab0025823ec1848b
Seunghwan-Seo1 <oosssok1@gmail.com>
2024-08-06 17:15:40 +0900

    day1

---
+++ hwfeday1-1/src/test/java/com/example/hwfeday1_1/Hwfeday11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hwfeday1_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hwfeday11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
