e68841f9ec46bd4ba5cd71f399952e2da0815905
Seunghwan-Seo1 <oosssok1@gmail.com>
2024-08-12 17:51:20 +0900

    day5

---
+++ fehwday5/src/main/java/com/example/fehwday5/Fehwday5Application.java
@@ -0,0 +1,13 @@
+package com.example.fehwday5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fehwday5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fehwday5Application.class, args);
+	}
+
+}
---
+++ fehwday5/src/test/java/com/example/fehwday5/Fehwday5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fehwday5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fehwday5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
